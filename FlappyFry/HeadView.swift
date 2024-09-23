da2487793bb0393666b9ade346bd1acf52b83e27
Andrey Sandler <mrdreem13@gmail.com>
2024-09-23 18:53:09 +0300

    Added head

---
+++ FlappyFry/HeadView.swift
@@ -0,0 +1,23 @@
+//
+//  HeadView.swift
+//  FlappyFry
+//
+//  Created by Andrey Sandler on 23.09.2024.
+//
+
+import SwiftUI
+
+struct HeadView: View {
+    let headSize: Double
+    
+    var body: some View {
+        Image(.head)
+            .resizable()
+            .scaledToFit()
+            .frame(width: headSize, height: headSize)
+    }
+}
+
+#Preview {
+    HeadView(headSize: 100)
+}
