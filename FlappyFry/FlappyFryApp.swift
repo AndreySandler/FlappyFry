25fa228eb8652352db51990712d46a94be06241a
Andrey Sandler <mrdreem13@gmail.com>
2024-09-23 18:05:34 +0300

    Initial Commit

---
+++ FlappyFry/FlappyFryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyFryApp.swift
+//  FlappyFry
+//
+//  Created by Andrey Sandler on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlappyFryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
