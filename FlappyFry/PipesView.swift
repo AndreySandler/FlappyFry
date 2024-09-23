194c80513910609df3134b97e51499a6c0cbca5d
Andrey Sandler <mrdreem13@gmail.com>
2024-09-23 19:07:09 +0300

    Added pipes

---
+++ FlappyFry/PipesView.swift
@@ -0,0 +1,18 @@
+//
+//  PipesView.swift
+//  FlappyFry
+//
+//  Created by Andrey Sandler on 23.09.2024.
+//
+
+import SwiftUI
+
+struct PipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PipesView()
+}
